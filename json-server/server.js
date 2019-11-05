4a513cf879275d7d29da1106327fdeada9eff7d8
Nikita Makarin <n-makarin@mail.ru>
2019-11-05 15:19:47 +0300

    Add server.js and routes.json

---
+++ json-server/server.js
@@ -0,0 +1,27 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const routes = require('./routes.json')
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// server.use((req, res, next) => {
+  // if (req.query.user_id) { // add your authorization logic here
+  //   next() // continue to JSON Server router
+  // } else {
+  //   console.log(req.query.user_id)
+  //   res.sendStatus(401)
+  // }
+// })
+
+
+// Add custom routes
+server.use(jsonServer.rewriter(routes))
+
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
