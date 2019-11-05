2b3b283bf924a7541d3b9ba18c7772914ec28287
Nikita Makarin <n-makarin@mail.ru>
2019-11-05 15:35:06 +0300

    Add admin.vue & api.vue pages

---
+++ pages/admin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="admin">
+    <nuxt-child />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      auth: null
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+</style>
