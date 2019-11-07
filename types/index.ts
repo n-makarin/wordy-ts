1616789d850188e2c929d140723586e114372f8b
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 20:52:24 +0300

    Change User interface in types\index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,13 @@
 export interface User {
-	firstName: string;
-  secondName: string;
-  age: number;
-  phone: number;
+  login: string;
+  email: string;
+  native_lang: string;
+  current_target_lang: string;
+	id?: number;
+  password?: string;
+  name?: string;
+  surname?: string;
+  date_of_birth?: number|null;
+  date_of_reg?: number|null;
+  admin?: boolean;
 }
