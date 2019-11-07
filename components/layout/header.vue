4ad58e82b28a3551771732b3bf1147ba7720c70b
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 13:50:31 +0300

    Create components\layout\header.vue

---
+++ components/layout/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="logout">
+    <logout />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Logout from '~/components/layout/header/logout.vue'
+
+export default Vue.extend({
+  components: {
+    Logout
+  },
+  methods: {
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
