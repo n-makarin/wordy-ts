cc03df729f7c3127eb1b5033325f1aa80e3366bc
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 18:47:27 +0300

    Change middleware\auth.js method to Auth class

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,60 @@
 import { cookies } from '~/store/config.ts'
 
-export default function ({ store, redirect, app }) {
-  let userFromStore = store.getters['auth/user']
-  const loginPath = '/login'
-  const joinPath = '/join'
-  const targetPath = app.context.route.fullPath
-
-  // try to get user from cookies
-  if (!userFromStore) {
+export class Auth {
+  constructor ({ store, app }) {
+    this.userFromStore = store.getters['auth/user']
+    this.loginPath = '/login'
+    this.joinPath = '/join'
+    this.targetPath = app.context.route.fullPath
+  }
+  /**
+   * Try to get user from cookies
+   * @returns void
+   */
+  setUserFromCookies ({ store, app }) {
     const userFromCookies = app.$cookies.get(cookies.authUser.name)
     if (userFromCookies) {
       store.commit('auth/SET_USER', userFromCookies)
-      userFromStore = userFromCookies
+      this.userFromStore = userFromCookies
     }
   }
-
-  // redirect to /login when no user and targetPath is not /login yet
-  if (!userFromStore && targetPath !== loginPath && targetPath !== joinPath) {
-    return redirect(loginPath)
+  /**
+   * @returns boolean
+   */
+  needRedirectToLogin () {
+    if (!this.userFromStore &&
+      this.targetPath !== this.loginPath &&
+      this.targetPath !== this.joinPath) {
+      return true
+    }
   }
-
-  // redirect to home page when have a user and targetPath is /login
-  if (userFromStore && (targetPath === loginPath || targetPath === joinPath)) {
-    return redirect('/')
+  /**
+   * @returns boolean
+   */
+  needRedirectToHome () {
+    if (this.userFromStore &&
+      (this.targetPath === this.loginPath ||
+      this.targetPath === this.joinPath)) {
+      return true
+    }
   }
+  /**
+   * Init authorization
+   * @returns redirection
+   */
+  init ({ store, redirect, app }) {
+    if (!this.userFromStore) {
+      this.setUserFromCookies({ store, app })
+    }
+    if (this.needRedirectToLogin()) {
+      return redirect(this.loginPath)
+    }
+    if (this.needRedirectToHome()) {
+      return redirect('/')
+    }
+  }
+}
+export default function ({ store, redirect, app }) {
+  const auth = new Auth({ store, app })
+  auth.init({ store, redirect, app })
 }
