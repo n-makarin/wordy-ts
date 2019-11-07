1db9fcabb6c99e26c317a6637df329381c8e3f81
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 13:51:58 +0300

    Add header.vue to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="def">
+    <Header />
     <nuxt />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '~/components/layout/header.vue'
+
+export default Vue.extend({
+  middleware: 'auth',
+  components: {
+    Header
+  }
+})
+</script>
 
 <style>
 </style>
