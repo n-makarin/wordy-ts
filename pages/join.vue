ab86441b9d7ceabebc8240deac3716bcb34fa630
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 20:57:17 +0300

    Create join form & createAccount method pages\join.vue

--- pages/join.vue
+++ pages/join.vue
@@ -1,21 +1,92 @@
 <template>
   <div class="join">
-    join
+    <h1>Create your account</h1>
+    <form class="join-form" @submit.prevent="createAccount">
+      <div
+        v-for="(item, key) in fieldList"
+        :key="key"
+      >
+        <label :for="key">{{ item.label }}</label>
+        <input
+          :id="key"
+          v-model="item.value"
+          :type="key === 'password' ? 'password' : 'text'"
+          :required="item.required"
+        >
+      </div>
+      <button>Create</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { User } from '~/types/index.ts'
 
 export default Vue.extend({
   data () {
     return {
-
+      fieldList: {
+        login: {
+          value: '',
+          label: 'Login',
+          tooltip: '',
+          required: true
+        },
+        email: {
+          value: '',
+          label: 'Email adress',
+          tooltip: '',
+          required: true
+        },
+        password: {
+          value: '',
+          label: 'Password',
+          tooltip: '',
+          required: true
+        },
+        nativeLang: {
+          value: '',
+          label: 'Native language',
+          tooltip: 'You can change this at any time',
+          required: true
+        },
+        targetLang: {
+          value: '',
+          label: 'Target language',
+          tooltip: 'You can change this at any time',
+          required: true
+        }
+      }
+    }
+  },
+  computed: {
+    authorized () {
+      return this.$store.getters['auth/user']
     }
   },
   methods: {
-    login (): void {
-
+    async createAccount (): Promise<void> {
+      const payload: User = {
+        login: '',
+        email: '',
+        native_lang: '',
+        current_target_lang: '',
+        password: '',
+        name: '',
+        surname: '',
+        date_of_birth: null,
+        date_of_reg: null
+      }
+      for (const key in this.fieldList) {
+        if (this.fieldList.hasOwnProperty(key)) {
+          // @ts-ignore
+          payload[key] = this.fieldList[key].value || 'something else'
+        }
+      }
+      await this.$store.dispatch('user/create', payload)
+      if (!this.authorized) { return }
+      this.$router.push('/')
     }
   }
 })
