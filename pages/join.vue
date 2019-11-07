258c8d8a8e0d7b05c3f0cab2f94c5c02d63af88f
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 13:00:21 +0300

    Create join.vue & login.vue pages

---
+++ pages/join.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="join">
+    join
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'empty',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    login (): void {
+
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+    <h1>Sign in</h1>
+    <form class="login-form" @submit.prevent="signIn">
+      <label for="login">{{ login.label }}</label>
+      <input id="login" v-model="login.data" type="text" required>
+
+      <label for="password">{{ password.label }}</label>
+      <input id="password" v-model="password.data" type="password" required>
+
+      <button>Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'empty',
+  data () {
+    return {
+      login: {
+        data: '',
+        label: 'Login or email address'
+      },
+      password: {
+        data: '',
+        label: 'Password'
+      }
+    }
+  },
+  methods: {
+    signIn (): void {
+      const authenticated: any = this.$store.dispatch('auth/login', {
+        login: this.login.data, password: this.password.data
+      })
+      if (authenticated) {
+        this.$router.push('/')
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
