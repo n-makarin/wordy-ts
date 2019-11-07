0af3f3b507461e80f1d922aa76ab8ccfe9c13c59
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 20:59:05 +0300

    Remove test store data in store\index.ts

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,8 @@
 export const state = () => ({
-  userName: 'Johny'
 })
 
 export const mutations = {
-  SET_USER_NAME (state: any, payload: any) {
-    state.userName = payload
-  }
 }
 
 export const getters = {
-  userName(state: any) {
-    return state.userName
-  } 
 }
