def84c10bcc47ce5312863a83dc77aa77fbd5f2e
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 18:46:43 +0300

    Create layouts\admin.vue Add it to pages\admin.vue

---
+++ layouts/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+
+export default Vue.extend({
+  middleware: 'auth-admin'
+})
+</script>
+
+<style>
+</style>
--- pages/admin.vue
+++ pages/admin.vue
@@ -8,7 +8,7 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  layout: 'empty',
+  layout: 'admin',
   data () {
     return {
       auth: null
