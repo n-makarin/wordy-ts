e3de6f8296da50003b7cf0939490aeebaa8e7d06
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 16:58:38 +0300

    Add middleware/auth.js logic

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,26 @@
-export default function ({ store, redirect }) {
-  const authUser = store.getters['auth/user']
-  if (!authUser) {
-    return redirect('/login')
+import { cookies } from '~/store/config.ts'
+
+export default function ({ store, redirect, app }) {
+  let userFromStore = store.getters['auth/user']
+  const loginPath = '/login'
+  const targetPath = app.context.route.fullPath
+
+  // try to get user from cookies
+  if (!userFromStore) {
+    const userFromCookies = app.$cookies.get(cookies.authUser.name)
+    if (userFromCookies) {
+      store.commit('auth/SET_USER', userFromCookies)
+      userFromStore = userFromCookies
+    }
+  }
+
+  // redirect to /login when no user and targetPath is not /login yet
+  if (!userFromStore && targetPath !== loginPath) {
+    return redirect(loginPath)
+  }
+
+  // redirect to home pahe when have a user and targetPath is /login
+  if (userFromStore && targetPath === loginPath) {
+    return redirect('/')
   }
 }
