3838e7251f8ab47749f096fc583e8209bf037876
Nikita Makarin <n-makarin@mail.ru>
2019-11-07 13:50:44 +0300

    Create components\layout\header\logout.vue

---
+++ components/layout/header/logout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="logout">
+    <button v-if="authorized" @click="logout">
+      {{ logOutText }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      logOutText: 'Sign out'
+    }
+  },
+  computed: {
+    authorized () {
+      return this.$store.getters['auth/user']
+    }
+  },
+  methods: {
+    logout (): void {
+      this.$store.dispatch('auth/logout')
+      if (!this.authorized) {
+        this.$router.push('/login')
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
