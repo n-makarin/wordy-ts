2e45415d42ca6538d5fb241a2604357cab1a4abb
Nikita Makarin <n-makarin@mail.ru>
2019-11-11 11:57:53 +0300

    Refactoring utils\form\validate.ts Change functions to extended classes

--- utils/form/validate.ts
+++ utils/form/validate.ts
@@ -1,91 +1,188 @@
-import * as Types from '~/types/utils/validate.ts'
+import * as ValidateTypes from '~/types/utils/form/validate.ts'
+import * as FieldTypes from '~/types/utils/form/field.ts'
 import { lettersAndNumbers, email } from '~/utils/regexp.ts'
 
-export default {
-  /**
-   * 
-   */
-  input(event: any, field: any): Types.Input {
-    const fieldMaxLength: number|null = field.length ? field.length.max : null
-    const keyCode: string = String.fromCharCode(event.keyCode);
 
-    if (lettersAndNumbers.test(keyCode)) {
-      // remove error message
-      if (field.error.visible) {
-        this.removeErrorMessage(field)
-      }
-      
-      // max length limit
-      if (fieldMaxLength && field.value.length >= fieldMaxLength) {
-        event.preventDefault()
-        return {
-          needToUpdate: false,
-          field: null
-        }
-      }
-    }
-    return {
-      needToUpdate: false,
-      field
-    }
-  },
-  /**
-   * 
-   */
-  removeErrorMessage (field: any): void {
-    field.error = {
+const emptyField: FieldTypes.Field = {
+  value: '',
+  label: '',
+  required: false,
+  error: {
+    visible: false,
+    message: ''
+  }
+}
+
+class Field implements ValidateTypes.Field {
+  field = emptyField
+  minRange = {
+    declared: false,
+    value: 0
+  }
+  maxRange = {
+    declared: false,
+    value: 0
+  }
+  constructor (field: FieldTypes.Field) {
+    this.field = field
+  }
+  removeErrorMessage (): void {
+    this.field.error = {
       visible: false,
       message: ''
     }
-  },
-  final(fieldList: object, Types: any) {
-    let valid: boolean = true
+  }
+  isEmail (value: string): boolean {
+    return email.test(value)
+  }
+  setRangeRestriction (field: FieldTypes.Field, range: string): void {
+    if (range === 'min') {
+      this.minRange = {
+        declared: Boolean(field.range),
+        value: field.range ? field.range.min : 0
+      }
+    } else if (range === 'max') {
+      this.maxRange = {
+        declared: Boolean(field.range),
+        value: field.range ? field.range.max : 0
+      }
+    }
+  }
+  inRange (range: string, onInput = false): boolean {
+    let fieldVelueLength: number = this.field.value.length
+    // increase field length, when char wasn't inserted
+    // to input field on input event
+    if (onInput) {
+      fieldVelueLength++
+    }
+    if (range === 'min') {
+      if (this.minRange.value > fieldVelueLength) { return false }
+    } else if (range === 'max') {
+      if (this.maxRange.value < fieldVelueLength) { return false }
+    }
+    return true
+  }
+}
+
+class FieldOnInput extends Field implements ValidateTypes.FieldOnInput {
+  keyCode = ''
+  isLetterOrNumberKeyCode = false
+  needToUpdate = false
+
+  super (field: FieldTypes.Field) {
+    this.setRangeRestriction(field, 'max')
+  }
+  input (event: any): ValidateTypes.InputFuncResult {
+    this.setKeyCodeType(event)
+    if (this.isLetterOrNumberKeyCode) {
+      this.makeValidation(event)
+    }
+    return {
+      needToUpdate: this.needToUpdate,
+      field: this.field
+    }
+  }
+  makeValidation (event: any): void {
+    this.hideError()
+    this.setRangeRestriction(this.field, 'max')
+    this.limitMaxRange(event)
+  }
+  setKeyCodeType (event: any): void {
+    this.keyCode = String.fromCharCode(event.keyCode)
+    this.isLetterOrNumberKeyCode = lettersAndNumbers.test(this.keyCode)
+  }
+  limitMaxRange (event: any): void {
+    if (!this.maxRange.declared) { return }
+    if (this.inRange('max', true)) { return }
+    event.preventDefault()
+    this.needToUpdate = false
+  }
+  hideError(): void {
+    if (this.field.error.visible) {
+      this.removeErrorMessage()
+    }
+  }
+}
+
+class FinalCheck extends Field implements ValidateTypes.FinalCheck {
+  valid = true
+  constructor() {
+    super(emptyField);
+  }
+  validate (fieldList: FieldTypes.List): ValidateTypes.FinalCheckResult {
     for (const key in fieldList) {
       if (fieldList.hasOwnProperty(key)) {
-        // @ts-ignore
-        const field: Types.Field = fieldList[key]
-        const fieldMinLength: number|null = field.length ? field.length.min : null
-  
-        // if required and empty
-        if (field.required && !field.value) {
-          valid = false
-          field.error = {
-            visible: true,
-            message: 'Field is required'
-          }
-        } else if (key === 'email' && !this.email(field.value)) {
-          valid = false
-          field.error = {
-            visible: true,
-            message: 'Invalid email'
-          }
-        // min length limit
-        } else if (fieldMinLength && field.value.length && field.value.length < fieldMinLength) {
-          valid = false
-          field.error = {
-            visible: true,
-            message: `Minimum field length is ${fieldMinLength} characters`
-          }
-        } else {
-          field.error = {
-            visible: false,
-            message: ''
-          }
+        this.field = fieldList[key]
+        this.setRangeRestriction(this.field, 'min')
+        this.setRangeRestriction(this.field, 'max')
+
+        this.required()
+        if (this.needToValidate()) {
+          this.email(key)
+          this.minLength()
+          this.maxLength()
         }
-  
-        // @ts-ignore
-        fieldList[key] = field
+
+        fieldList[key] = this.field
       }
     }
     return {
-      valid: valid,
-      errorsComplemented: fieldList
+      valid: this.valid,
+      dataWithErrors: fieldList
     }
+  }
+  required (): void {
+    if (!this.field.required) { return }
+    if (this.field.value) { return }
+    this.valid = false
+    this.field.error = {
+      visible: true,
+      message: 'Field is required'
+    }
+  }
+  email (key: string): void {
+    if (key === 'email' && !this.isEmail(this.field.value)) {
+      this.valid = false
+      this.field.error = {
+        visible: true,
+        message: 'Invalid email'
+      }
+    }
+  }
+  minLength (): void {
+    if (!this.minRange.declared) { return }
+    // if (this.field.required && !this.field.value.length) { return }
+    if (this.inRange('min')) { return }
+    this.valid = false
+    this.field.error = {
+      visible: true,
+      message: `Minimum field length is ${this.minRange.value} characters`
+    }
+  }
+  maxLength (): void {
+    if (!this.maxRange.declared) { return }
+    if (this.inRange('max')) { return }
+    
+    this.valid = false
+    this.field.error = {
+      visible: true,
+      message: `Maximum field length is ${this.maxRange.value} characters`
+    }
+  }
+  needToValidate (): boolean {
+    if (!this.field.value) { return false }
+    if (!this.field.required && this.field.value) { return true }
+    return true
+  }
+}
+
+export default {
+  onInput(field: FieldTypes.Field, event: any): ValidateTypes.InputFuncResult {
+    const init = new FieldOnInput(field)
+    return init.input(event)
   },
-  /**
-   * 
-   */
-  email(value: string): boolean {
-    return email.test(value)
+  finalCheck(fieldList: FieldTypes.List): ValidateTypes.FinalCheckResult {
+    const init = new FinalCheck()
+    return init.validate(fieldList)
   }
 }
