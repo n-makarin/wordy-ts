4890b45b40807652919d8fd65bcd686a7ee6ef9d
Nikita Makarin <n-makarin@mail.ru>
2019-11-12 11:18:54 +0300

    Add isUniqueField async method Add hasError method, login async method utils\form\validate\final-check.ts

--- utils/form/validate/final-check.ts
+++ utils/form/validate/final-check.ts
@@ -2,13 +2,15 @@ import * as ValidateTypes from '~/types/utils/form/validate.ts'
 import * as FieldTypes from '~/types/utils/form/field.ts'
 import { Field } from '~/utils/form/validate/field.ts'
 import { emptyField } from '~/utils/form/validate/constants.ts'
+import axios from 'axios'
+import { endpoint } from '~/app-config.ts'
 
 export class FinalCheck extends Field implements ValidateTypes.FinalCheck {
   valid = true
   constructor() {
     super(emptyField);
   }
-  validate (fieldList: FieldTypes.List): ValidateTypes.FinalCheckResult {
+  async validate (fieldList: FieldTypes.List): Promise<ValidateTypes.FinalCheckResult> {
     for (const key in fieldList) {
       if (fieldList.hasOwnProperty(key)) {
         this.field = fieldList[key]
@@ -17,9 +19,10 @@ export class FinalCheck extends Field implements ValidateTypes.FinalCheck {
 
         this.required()
         if (this.needToValidate()) {
-          this.email(key)
           this.minLength()
           this.maxLength()
+          await this.login(key)
+          await this.email(key)
         }
 
         fieldList[key] = this.field
@@ -39,18 +42,40 @@ export class FinalCheck extends Field implements ValidateTypes.FinalCheck {
       message: 'Field is required'
     }
   }
-  email (key: string): void {
-    if (key === 'email' && !this.isEmail(this.field.value)) {
+  async email (key: string): Promise<void> {
+    if (key !== 'email') { return }
+    if (this.hasError()) { return }
+    if (!this.isEmail(this.field.value)) {
       this.valid = false
       this.field.error = {
         visible: true,
         message: 'Invalid email'
       }
+      return
+    }
+    const isUnique: boolean = await this.isUniqueField(key, this.field.value)
+    if (!isUnique) {
+      this.valid = false
+      this.field.error = {
+        visible: true,
+        message: `Email ${this.field.value} is already exist`
+      }
+    }
+  }
+  async login (key: string): Promise<void> {
+    if (key !== 'login') { return }
+    if (this.hasError()) { return }
+    const isUnique: boolean = await this.isUniqueField(key, this.field.value)
+    if (!isUnique) {
+      this.valid = false
+      this.field.error = {
+        visible: true,
+        message: `Login ${this.field.value} is already exist`
+      }
     }
   }
   minLength (): void {
     if (!this.minRange.declared) { return }
-    // if (this.field.required && !this.field.value.length) { return }
     if (this.inRange('min')) { return }
     this.valid = false
     this.field.error = {
@@ -68,9 +93,27 @@ export class FinalCheck extends Field implements ValidateTypes.FinalCheck {
       message: `Maximum field length is ${this.maxRange.value} characters`
     }
   }
+  hasError (): boolean {
+    if (this.field.error.visible) { return true }
+    return false
+  }
   needToValidate (): boolean {
     if (!this.field.value) { return false }
     if (!this.field.required && this.field.value) { return true }
     return true
+  }
+  async isUniqueField (field: string, value: string): Promise<boolean> {
+    const response: boolean = await axios({
+      method: 'GET',
+      url: `${endpoint}/user/detail/?${field}=${value}`
+    })
+      .catch((err: any) => {
+        console.log(err)
+        return false
+      })
+      .then((response: any) => {
+        return !response || !response.data || response.data.length === 0
+      })
+    return response
   }
 }
