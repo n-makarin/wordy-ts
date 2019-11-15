30564a2f7f4cb11694228a4bc31d47da4a4e2401
Nikita Makarin <n-makarin@mail.ru>
2019-11-15 12:47:43 +0300

    Add edit() action to store\user\actions.ts

--- store/user/actions.ts
+++ store/user/actions.ts
@@ -1,4 +1,5 @@
 import { cookies } from '~/store/config.ts'
+import { User as UserTypes } from '~/types/index.ts'
 
 export default {
   async create({}, payload: object): Promise<void> {
@@ -21,5 +22,28 @@ export default {
             maxAge: cookies.authUser.maxAge
           })
         })
+  },
+  async edit({}, payload: UserTypes): Promise<boolean> {
+    // @ts-ignore
+    const response: any = await this.app.$sendRequest({
+      method: 'PATCH',
+      url: `/user/${payload.id}`,
+      data: payload
+    })
+      .catch((err: any) => {
+        console.log(err)
+      })
+      .then((response: any) => {
+        if (!response || !response.data || response.data.length === 0) { return false }
+        const user: any = response.data
+        // @ts-ignore
+        this.app.store.commit('auth/SET_USER', user)
+        // @ts-ignore
+        this.app.$cookies.set(cookies.authUser.name, user, {
+          maxAge: cookies.authUser.maxAge
+        })
+        return true
+      })
+    return response
   }
 }
